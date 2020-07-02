768282f9cd40af3ec4e05b986c705b6f42f0f9e7
Kübay Telçeken <kubaytelceken@users.noreply.github.com>
2020-07-02 19:10:47 +0300

    Add project files.

---
+++ WebApiProject/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiProject.DAL.Context;
+using WebApiProject.DAL.Entities;
+
+namespace WebApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using var context = new WebApiContext();
+            return Ok(context.Categories.ToList());
+        }
+    }
+}
---
+++ WebApiProject/DAL/Context/WebApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiProject.DAL.Entities;
+
+namespace WebApiProject.DAL.Context
+{
+    public class WebApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb; database=WebApiDB; integrated security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
